22a06083bc72f6fc64d0dbd3d7669ae14d167223
CaptainRong <cr1092014130@gmail.com>
2024-01-06 18:38:53 +0800

    新增index页面。快跑，傻孩子！

--- Retrieval/views.py
+++ Retrieval/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     print("try to connect")
-    return HttpResponse('<h1>Hello World!</h1>')
+    return render(request,'index.html')
