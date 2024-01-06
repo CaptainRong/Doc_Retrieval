4942a46bc273de7815b72ec3e3e86159af6957b6
CaptainRong <cr1092014130@gmail.com>
2024-01-06 17:05:13 +0800

    init1

--- Retrieval/views.py
+++ Retrieval/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
 def index(request):
+    print("try to connect")
     return HttpResponse('<h1>Hello World!</h1>')
